30b44566df1168811d213567aedc1604ee2110ad
Erik Huang <ekhu94@gmail.com>
2021-04-03 08:03:44 -0700

    add action creator and call in componentDidMount

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
         return (
             <div>Post List</div>
@@ -8,4 +15,8 @@ class PostList extends React.Component {
     }
 }
 
-export default PostList;
+const mapStateToProps = state => {
+    
+};
+
+export default connect(null, { fetchPosts })(PostList);
