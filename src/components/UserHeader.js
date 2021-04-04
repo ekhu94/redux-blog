0fa4f56ab336356c82e327c2d82863cf6ae8c271
Erik Huang <ekhu94@gmail.com>
2021-04-03 23:49:51 -0700

    refactor logic into mapStateToProps

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -2,27 +2,26 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
 
-const UserHeader = ({ userId, fetchUser, users }) => {
+const UserHeader = ({ userId, fetchUser, user }) => {
 
     useEffect(() => {
         fetchUser(userId);
     }, []);
 
-    const getUser = () => {
-        const user = users.find(u => u.id === userId);
+    const renderUser = () => {
         if (!user) return null;
         return user.name;
-    }
+    };
 
     return (
         <div className="header">
-            {getUser()}
+            {renderUser()}
         </div>
     );
 };
 
-const mapStateToProps = state => {
-    return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+    return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
