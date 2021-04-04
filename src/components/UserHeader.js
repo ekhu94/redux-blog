cc45855ba0e78092c65abe00a3302e2865b474e7
Erik Huang <ekhu94@gmail.com>
2021-04-03 23:38:21 -0700

    finds and renders user to each post

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -2,15 +2,21 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
 
-const UserHeader = ({ userId, fetchUser }) => {
+const UserHeader = ({ userId, fetchUser, users }) => {
 
     useEffect(() => {
         fetchUser(userId);
     }, []);
 
+    const getUser = () => {
+        const user = users.find(u => u.id === userId);
+        if (!user) return null;
+        return user.name;
+    }
+
     return (
-        <div>
-            User Header
+        <div className="header">
+            {getUser()}
         </div>
     );
 };
